34cd12c313d44dbfa96da24b7b47ca4c61204573
Lina <dev@lina.moe>
2023-12-26 16:38:40 +0200

    Write an empty image

--- voxels/main.cpp
+++ voxels/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+
+#include <glm/glm.hpp>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 int main() {
+    constexpr int width = 1024;
+    constexpr int height = 768;
+
     std::cout << "Hello, World!" << std::endl;
+
+    std::vector<glm::vec3> framebuffer;
+    framebuffer.resize(width * height);
+
+    stbi_write_png("out.png", width, height, 3, framebuffer.data(), 1920 * sizeof(glm::vec3));
     return 0;
 }
